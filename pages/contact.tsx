0c6039b753a561bf873e0ebc36ee8256f15a1f2d
John_Lamy <johnclamy@gmail.com>
2023-03-22 22:37:36 +0000

    setup home and contact pages

---
+++ pages/contact.tsx
@@ -0,0 +1,11 @@
+export default function ContactPage() {
+  return (
+    <div>
+      <ul>
+        <li> Email: someone@hisemail.com</li>
+        <li> Twitter: @someone</li>
+        <li> Instagram: thatsomeone</li>
+      </ul>
+    </div>
+  );
+}
