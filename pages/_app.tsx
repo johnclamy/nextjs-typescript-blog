bef8ca0e9cc31abc3490d26d385aaec96b2a9334
John_Lamy <johnclamy@gmail.com>
2023-03-28 23:38:31 +0100

    setup bootstrap for styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: any) {
+  return <Component {...pageProps} />;
 }
