c0818408398338c9e5cfb7e039828f8c42129307
John_Lamy <johnclamy@gmail.com>
2023-03-29 01:54:23 +0100

    setup the header component

---
+++ pages/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="my-3">
+      <h1 className="h4 text-center">
+        Hello and Welcome 👏&nbsp;
+        <span className="badge bg-light text-dark">I am John</span>
+      </h1>
+    </header>
+  );
+}
