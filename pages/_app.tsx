e30d4500d3e6121fad5e93e4c5b4ab000a9a6bef
John_Lamy <johnclamy@gmail.com>
2023-03-29 14:59:17 +0100

    define the layout in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
+import { PropsWithChildren } from "react";
+import { AppProps } from "next/app";
+import PageWithLayoutType from "./components/PageWithLayout";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />;
+type AppLayoutProps = AppProps & {
+  Component: PageWithLayoutType;
+  pageProps: any;
+};
+
+export default function MyApp({ Component, pageProps }: AppLayoutProps) {
+  const Layout =
+    Component.layout || ((children: PropsWithChildren) => <>{children}</>);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
