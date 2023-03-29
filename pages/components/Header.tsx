37d636e72bc39bab62e3332e8cae31473682da67
John_Lamy <johnclamy@gmail.com>
2023-03-29 10:49:48 +0100

    add img to header

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,9 +1,12 @@
+import HeaderImage from "./HeaderImage";
+
 export default function Header() {
   return (
     <header className="my-3">
-      <h1 className="h4 text-center">
+      <HeaderImage />
+      <h1 className="h5 text-center mt-4">
         Hello and Welcome 👏&nbsp;
-        <span className="badge bg-light text-dark">I am John</span>
+        <span className="badge bg-light text-dark">{`I'm John`}</span>
       </h1>
     </header>
   );
