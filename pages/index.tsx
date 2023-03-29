34f2996b7bf45c62db25d71d24a1e5400893239e
John_Lamy <johnclamy@gmail.com>
2023-03-29 10:50:34 +0100

    add navbar to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
 
 export default function HomePage() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-8 offset-md-2">
-          <Header />
+    <>
+      <div className="bg-light">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-10">
+              <Navigation />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <Header />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
