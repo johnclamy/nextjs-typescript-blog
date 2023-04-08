6e4459f6dd15c1eb806a539fa75834a0bd8337d0
John_Lamy <johnclamy@gmail.com>
2023-04-08 18:20:55 +0100

    add bootstrap to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: any) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />;
 }
