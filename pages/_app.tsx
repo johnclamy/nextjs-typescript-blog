1d3ebb0bbe526a0c61b18e4fc5f87bc15c7949af
John_Lamy <johnclamy@gmail.com>
2023-04-08 19:05:30 +0100

    check that boostrap bundle is not undefined

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,10 @@ import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: any) {
   useEffect(() => {
-    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    //import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    typeof document !== undefined
+      ? require("bootstrap/dist/js/bootstrap.bundle.min.js")
+      : null;
   }, []);
 
   return <Component {...pageProps} />;
