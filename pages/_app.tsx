da5413054af35c18e313d5b9d9bd3efd26f25c7c
John_Lamy <johnclamy@gmail.com>
2023-04-08 19:07:14 +0100

    remove comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: any) {
   useEffect(() => {
-    //import("bootstrap/dist/js/bootstrap.bundle.min.js");
     typeof document !== undefined
       ? require("bootstrap/dist/js/bootstrap.bundle.min.js")
       : null;
